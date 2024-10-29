0611f314c655ab257fe539a095eb069b37be8ec2
Altynay <altynaytileumuratova@gmail.com>
2024-10-29 12:31:23 +0500

    Add solution Work3

---
+++ work3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход 
+// целое число из отрезка [10, 99] и показывает 
+// наибольшую цифру числа.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(10 < number && number < 99)
+{
+    int leftDigit = number / 10;
+    int rightDigit = number % 10;
+    if(leftDigit > rightDigit)
+    {
+        System.Console.WriteLine(leftDigit);
+    }
+    else
+    {
+         System.Console.WriteLine(rightDigit);
+    }
+}
+else
+{
+    System.Console.WriteLine("Число не из отрезка [10, 99]");
+}
