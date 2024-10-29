aff0c67452157ddbfba6dd15600b08486cf48f88
Altynay <altynaytileumuratova@gmail.com>
2024-10-29 12:41:26 +0500

    Add solution Work4

---
+++ work4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает 
+// натуральное число N, а на выходе показывает его 
+// цифры через запятую.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+while (N > 0) 
+{ 
+    int res = N % 10; 
+     N /= 10; 
+    if (N > 0) 
+    { 
+         Console.Write(res + ", "); 
+    } 
+    else 
+    { 
+        Console.WriteLine(res); 
+    } 
+} 
