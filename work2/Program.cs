dda6cf727296239debadc0854eadd335313a02a1
Altynay <altynaytileumuratova@gmail.com>
2024-10-29 12:43:17 +0500

    Add solution Work2

--- work2/Program.cs
+++ work2/Program.cs
@@ -3,9 +3,25 @@
 // и выдаёт номер координатной четверти плоскости,
 // в которой находится эта точка.
 
-Console.Write("Введите первую координату: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты X:");
+int X = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите вторую координату: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение координаты Y:");
+int Y = Convert.ToInt32(Console.ReadLine());
 
+if(X > 0 && Y > 0)
+{
+    Console.WriteLine("На первой четверти");
+}
+else if(X < 0 && Y > 0) 
+{
+    Console.WriteLine("На второй четверти");
+}
+else if(X < 0 && Y < 0) 
+{
+    Console.WriteLine("На третьей четверти");
+}
+else if(X > 0 && Y < 0) 
+{
+    Console.WriteLine("На четвертой четверти");
+}
